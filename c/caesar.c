c936452ecd8248d47b21a15073c36d84583c0253
twelve <kana.suzucki@gmail.com>
2022-10-26 23:50:08 +0300

    new: ceasar.c

---
+++ c/caesar.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+
+
+char* caesar_encryption(const char* text, const size_t key)
+{
+    const size_t tlength = strlen(text);
+    char* result = (char*)malloc(tlength);
+
+    for (register int i = 0; i < tlength; i++)
+    {
+        result[i] = (text[i] - (isupper(text[i]) == 0 ? 97 : 65) + key) % 26 + (isupper(text[i]) == 0 ? 97 : 65);
+    }
+
+    return result;
+}
+
+
+char* caesar_decryption(const char* text, const size_t key)
+{
+    const size_t tlength = strlen(text);
+    char* result = (char*)malloc(tlength);
+
+    for (register int i = 0; i < tlength; i++)
+    {
+        result[i] = (text[i] - (isupper(text[i]) == 0 ? 97 : 65) + 26 - key) % 26 + (isupper(text[i]) == 0 ? 97 : 65);
+    }
+
+    return result;
+}
+
+
+int main(int argc, char* argv[])
+{
+    assert(strcmp(caesar_encryption("CRYptograpHY", 3), "FUBswrjudsKB") == 0);
+    assert(strcmp(caesar_decryption("FUBswrjudsKB", 3), "CRYptograpHY") == 0);
+
+    return 0;
+}
--- python/caesar.py
+++ python/caesar.py
@@ -7,77 +7,20 @@ caesar: caesar algorithm. Formula:
 """
 
 
-def caesar_upper_encryption(text: str, key: int = 3) -> str:
-    """
-    caesar_upper_encryption: caesar encryption algorithm for english
-    alphabet in uppercase.
-    """
-    return ''.join([chr(((ord(x) - 65 + key) % 26) + 65) for x in text])
-
-
-def caesar_upper_decryption(text: str, key: int = 3) -> str:
-    """
-    caesar_upper_decryption: caesar decryption algorithm for english
-    alphabet in uppercase.
-    """
-    return ''.join([chr(((ord(x) - 65 + 26 - key) % 26) + 65) for x in text])
-
-
-def caesar_lower_encryption(text: str, key: int = 3) -> str:
-    """
-    caesar_lower_encryption: caesar encryption algorithm for english
-    alphabet in lowercase.
-    """
-    return ''.join([chr(((ord(x) - 97 + key) % 26) + 97) for x in text])
-
-
-def caesar_lower_decryption(text: str, key: int = 3) -> str:
-    """
-    caesar_lower_decryption: caesar decryption algorithm for english
-    alphabet in lowercase.
-    """
-    return ''.join([chr(((ord(x) - 97 + 26 - key) % 26) + 97) for x in text])
-
-
 def caesar_encryption(text: str, key: int = 3) -> str:
     """
-    caesar_encryption: caesar encryption algorithm for english
-    alphabet in any register.
+    caesar_encryption: caesar encryption algorithm for english alphabet in any register.
     """
     return ''.join([chr(((ord(x) - (65 if x.isupper() else 97) + key) % 26) + (65 if x.isupper() else 97)) for x in text])
 
 
 def caesar_decryption(text: str, key: int = 3) -> str:
     """
-    caesar_decryption: caesar decryption algorithm for english
-    alphabet in any register.
+    caesar_decryption: caesar decryption algorithm for english alphabet in any register.
     """
     return ''.join([chr(((ord(x) - (65 if x.isupper() else 97) + 26 - key) % 26) + (65 if x.isupper() else 97)) for x in text])
 
 
-def caesar_ascii_encryption(text: str, key: int = 3) -> str:
-    """
-     caesar_ascii_encryption: caesar encryption algorithm for ascii symbols.
-    """
-    return ''.join([chr((ord(x) + key) % 256) for x in text])
-
-
-def caesar_ascii_decryption(text: str, key: int = 3) -> str:
-    """
-    caesar_ascii_decryption: caesar decryption algorithm for ascii symbols.
-    """
-    return ''.join([chr((ord(x) + 256 - key) % 256) for x in text])
-
-
 if __name__ == '__main__':
-    assert caesar_upper_encryption('CRYPTOGRAPHY') == 'FUBSWRJUDSKB'
-    assert caesar_upper_decryption('FUBSWRJUDSKB') == 'CRYPTOGRAPHY'
-
-    assert caesar_lower_encryption('cryptography') == 'fubswrjudskb'
-    assert caesar_lower_decryption('fubswrjudskb') == 'cryptography'
-
     assert caesar_encryption('cryPTOgraphy') == 'fubSWRjudskb'
     assert caesar_decryption('fubSWRjudskb') == 'cryPTOgraphy'
-
-    assert caesar_ascii_encryption('cryPTOgraphy') == 'fu|SWRjudsk|'
-    assert caesar_ascii_decryption('fu|SWRjudsk|') == 'cryPTOgraphy'
