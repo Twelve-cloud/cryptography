5bfc555b40d2cb0284ce6f3a4bce8364df09a946
twelve <kana.suzucki@gmail.com>
2022-10-26 23:53:41 +0300

    new: caesar.c

--- c/caesar.c
+++ c/caesar.c
@@ -37,5 +37,6 @@ int main(int argc, char* argv[])
 {
     assert(strcmp(caesar_encryption("CRYptograpHY", 3), "FUBswrjudsKB") == 0);
     assert(strcmp(caesar_decryption("FUBswrjudsKB", 3), "CRYptograpHY") == 0);
+    
     return 0;
 }
